c326d6dc0b774468122ff32a42a2ca1aa2db142c
lottotto <kentaro.saito50@gmail.com>
2020-03-10 01:13:24 +0900

    add S3-bucket

--- src/src/src_stack.py
+++ src/src/src_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core
+from aws_cdk import aws_s3 as s3
 
 
 class SrcStack(core.Stack):
@@ -7,3 +8,4 @@ class SrcStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True,)
